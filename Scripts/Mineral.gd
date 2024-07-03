b07b770ffe311da969a1ff637e711956ff8a5431
Strikerfury222 <71235406+Strikerfury222@users.noreply.github.com>
2024-07-03 19:38:29 +0200

    Implementado corrupción por luz y picado
    Falta que se transporte y la corrupción por oscuridad

---
+++ Scripts/Mineral.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+class_name Mineral
+
+@export var vidaMax: int = 3
+@onready var vida: int = vidaMax 
+
+func eliminarme():
+	print("I WAS PLAYED")
+	self.queue_free()
+
+func picar() -> bool:
+	print("OUCH")
+	vida -= 1
+	if vida > 0:
+		return false
+	else:
+		return true
