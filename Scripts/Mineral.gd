b119f68465bfe70145ef8b09e1f5e0edd4405b96
Strikerfury222 <71235406+Strikerfury222@users.noreply.github.com>
2024-07-04 17:38:07 +0200

    Añadido cambio de sprite

--- Scripts/Mineral.gd
+++ Scripts/Mineral.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 class_name Mineral
 
-@export var vidaMax: int = 3
+@export var vidaMax: int = 5
 @onready var vida: int = vidaMax 
 @onready var carried: bool = false
 @onready var value: int = 5
@@ -19,7 +19,7 @@ func eliminarme():
 	self.queue_free()
 
 func picar() -> bool:
-	print("OUCH")
+	#print("OUCH")
 	vida -= 1
 	if vida > 0:
 		return false
--- Scripts/RecolectarCristales.gd
+++ Scripts/RecolectarCristales.gd
@@ -4,11 +4,35 @@ extends Area3D
 @onready var goalCristales: int = 100 
 @onready var text := $"../SubViewport/NumCristales"
 @onready var viewPort := $"../SubViewport"
+@onready var spriteNave := $"../SpriteNave"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	body_entered.connect(collectCarry)
 	viewPort.size = Vector2(19,40)
 	text.set_text(str(numCristales))
+
+func _process(delta):
+	var porcentaje: float = float(numCristales)/goalCristales
+	if porcentaje < 10:
+		spriteNave.frame = 0
+	elif porcentaje < 20:
+		spriteNave.frame = 1
+	elif porcentaje < 30:
+		spriteNave.frame = 2
+	elif porcentaje < 40:
+		spriteNave.frame = 3
+	elif porcentaje < 50:
+		spriteNave.frame = 4
+	elif porcentaje < 60:
+		spriteNave.frame = 5
+	elif porcentaje < 70:
+		spriteNave.frame = 6
+	elif porcentaje < 80:
+		spriteNave.frame = 7
+	elif porcentaje < 90:
+		spriteNave.frame = 8
+	else:
+		spriteNave.frame = 9
 
 func collectCarry(body):
 	if body.carry:
